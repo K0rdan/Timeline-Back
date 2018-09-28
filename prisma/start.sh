a7628c1d68e8751ffa301c249496986755a95055
Benjamin Christau <benjamin.christau@oyez.fr>
2018-09-28 16:14:48 +0200

    delete unused files and prisma files, that are now in their own repo

--- prisma/start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# OVERRIDE DEFAULT IMAGE start.sh
-
-set -e
-cd /app
-echo "Waiting MySQL service (tcp://$MYSQL_HOST:$MYSQL_PORT) to run PRISMA server on http://localhost:${PRISMA_PORT} ..."
-dockerize --wait tcp://$MYSQL_HOST:$MYSQL_PORT --timeout 60s ./bin/prisma-local &
-dockerize --wait http://localhost:${PRISMA_PORT} --timeout 60s prisma deploy
-wait
